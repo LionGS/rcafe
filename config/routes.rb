7ca652f1458e150103abc961b6fd8d262c1528ff
LionGS <gb1541@likelion.org>
2017-05-06 10:36:36 +0900

    generate post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
+  root 'welcome#index' #root 정의는 항상 routes.rb 최상단에 위치해야한다.
+  #get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
