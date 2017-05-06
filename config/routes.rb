1fad4d4d0b326dcad5fa7911cee432292be9dd66
LionGS <gb1541@likelion.org>
2017-05-07 00:22:42 +0900

    edit posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :bulletins
+  resources :bulletins do
+    resources :posts
+  end
   resources :posts
-  root 'welcome#index' #root 정의는 항상 routes.rb 최상단에 위치해야한다.
-  #get 'welcome/index'
+  root 'welcome#index' # root 정의는 항상 routes.rb 최상단에 위치해야한다.
+  # get 'welcome/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
